aad1324de92bcd3987f8d6fca8e38aeada13a758
Kunal30 <kunalvsuthar@gmail.com>
2019-04-04 00:15:16 -0700

    First Commit

---
+++ tools/run_main.py
@@ -0,0 +1,135 @@
+# Code Author: Kunal Vinay Kumar Suthar
+# ASU ID: 1215112535
+# Course: CSE-573: Semantic Web Mining
+# Project: Document Clustering and Visualization
+
+# 1) ----> Data preprocessing(Tokenization, Stemming, Stopword Removal, Lematization) 
+# 2) ----> Latent Dirichlet Allocation 
+# 3) ----> TSNE 
+# 4) ----> 2D Visualization 
+# 5) ----> 3D Visualization
+
+
+from sklearn.datasets import fetch_20newsgroups
+from pprint import pprint
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import metrics
+from sklearn.naive_bayes import MultinomialNB
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+import numpy as np
+import nltk
+import pandas as pd
+from sklearn.manifold import TSNE
+import time
+from tsne import tsne
+
+'''
+Write a function to perform the pre processing steps on the entire dataset
+'''
+def lemmatize_stemming(text):
+    stemmer= SnowballStemmer("english")
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+
+# Tokenize and lemmatize
+def preprocess(text):
+    result=[]
+    for token in gensim.utils.simple_preprocess(text) :
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+            
+    return result
+
+def main():
+	
+	# Fetching train and testing data using sklearn's API
+
+	newsgroups_train = fetch_20newsgroups(subset='train')
+	newsgroups_test = fetch_20newsgroups(subset='test')
+	nltk.download('wordnet')
+				
+	#Preprocessing the training data
+	processed_docs = []
+	
+	for doc in newsgroups_train.data:
+		processed_docs.append(preprocess(doc))
+
+	'''
+	Create a dictionary from 'processed_docs' containing the number of times a word appears 
+	in the training set using gensim.corpora.Dictionary and call it 'dictionary'
+	'''
+	dictionary = gensim.corpora.Dictionary(processed_docs)
+
+	# '''
+	# Checking dictionary created
+	# '''
+	# count = 0
+	# for k, v in dictionary.iteritems():
+	#     print(k, v)
+	#     count += 1
+	#     if count > 10:
+	#         break
+
+	'''
+	Create the Bag-of-words model for each document i.e for each document we create a dictionary reporting how many
+	words and how many times those words appear. Save this to 'bow_corpus'
+	'''
+	bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+	
+	'''
+	Preview BOW for our sample preprocessed document
+	'''
+	# document_num = 20
+	# bow_doc_x = bow_corpus[document_num]
+
+	# for i in range(len(bow_doc_x)):
+	#     print("Word {} (\"{}\") appears {} time.".format(bow_doc_x[i][0], 
+	#                                                      dictionary[bow_doc_x[i][0]], 
+	#                                                      bow_doc_x[i][1]))        
+	
+	'''
+	Train your lda model using gensim.models.LdaMulticore and save it to 'lda_model'
+	'''
+	# TODO
+	lda_model =  gensim.models.LdaMulticore(bow_corpus, 
+	                                   num_topics = 20, 
+	                                   id2word = dictionary,                                    
+	                                   passes = 10,
+	                                   workers = 2)
+
+
+	'''
+	For each topic, we will explore the words occuring in that topic and its relative weight
+	'''
+	for idx, topic in lda_model.print_topics(-1):
+	    print("Topic: {} \nWords: {}".format(idx, topic ))
+	    print("\n")
+
+	# num = 100
+	# unseen_documents=[]
+	# for i in range(0,len(newsgroups_test.data)):
+	# 	unseen_documents.append(newsgroups_test.data[i])
+	
+	# print(unseen_documents)    
+
+	# Data preprocessing step for the unseen document
+	for doc in newsgroups_test.data:
+		bow_vector = dictionary.doc2bow(preprocess(doc))
+		print("*********************************************************************")
+		print(doc)
+		print("*********************************************************************")
+		for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1*tup[1]):
+		    print("Score: {}\t Topic: {}".format(score, lda_model.print_topic(index, 5)))
+
+	
+	print(lda_model)
+	# Now passing the Topic-Document Matrix to the TSNE MODULE:
+	tsne(lda_model)
+
+
+
+if __name__ == "__main__":
+	main()	
